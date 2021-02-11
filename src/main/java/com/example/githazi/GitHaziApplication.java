89aaae3dc368f6946655aa0952db3c62039d64ec
unknown <luterdav@gmail.com>
2021-02-11 21:01:08 +0100

    First Commit

---
+++ src/main/java/com/example/githazi/GitHaziApplication.java
@@ -0,0 +1,13 @@
+package com.example.githazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHaziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHaziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githazi/GitHaziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHaziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
